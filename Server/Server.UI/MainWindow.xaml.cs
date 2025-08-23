ed180f9808536be1a0eaf91ef0931ce5ee392b91
Franciszek Gwarek <130841500+franek73@users.noreply.github.com>
2025-08-24 00:20:15 +0200

    bases for server gui (#28)

--- Server/Server.UI/MainWindow.xaml.cs
+++ Server/Server.UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Server.UI.ViewModels;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,7 @@ namespace Server.UI
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
---
+++ Server/Server.UI/TabTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using Server.UI.Models;
+using Server.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Server.UI
+{
+    internal class TabTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate ServerTemplate { get; set; }
+        public DataTemplate LobbyTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is ServerViewModel)
+                return ServerTemplate;
+            else if (item is LobbyViewModel)
+                return LobbyTemplate;
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
