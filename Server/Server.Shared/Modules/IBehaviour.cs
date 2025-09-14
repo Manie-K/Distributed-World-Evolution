6c3684b3a1f472994759a19e36071a55ea96feab
Maciej Góralczyk <goralczykmaciek@gmail.com>
2025-09-15 01:12:52 +0200

    Add important comment.

--- Server/Server.Shared/Modules/IBehaviour.cs
+++ Server/Server.Shared/Modules/IBehaviour.cs
@@ -1,5 +1,11 @@
 ﻿namespace Server.Shared.Modules
 {
+    // VERY IMPORTANT TODO: Currently, each entity will require a seperate behaviour instance.
+    // This is not memory efficient. We will need to unload it to client somehow.
+    // Another way to solve this is to make behaviours static and stateless.
+    // WE NEED TO COME BACK TO THIS LATER.
+    // This is a very important design decision.
+    // To accomodate it, we need to keep all implementations state/fieldless for now.
     public interface IBehaviour
     {
         // IMPORTANT! If this ID is changed or duplicated it will break the module system.
