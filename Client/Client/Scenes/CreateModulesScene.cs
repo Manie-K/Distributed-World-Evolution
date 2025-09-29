39f387b568fdfc535c3abfbd0719d45948927310
Punio3 <czlapset@wp.pl>
2025-09-29 17:02:48 +0200

    Modules creating assets

---
+++ Client/Client/Scenes/CreateModulesScene.cs
@@ -0,0 +1,79 @@
+﻿using Client.UI.CreateLobby.Parameters;
+using Client.UI.MapSelection;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SharedLibrary.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Client
+{
+    public class CreateModulesScene : IScene
+    {
+        private GameManager manager;
+
+        private Texture2D backGround;
+        private SwitchPageModules switchPageModules;
+        private Button exitButton;
+        private Button saveButton;
+
+
+
+        public CreateModulesScene(GameManager manager)
+        {
+            this.manager = manager;
+            exitButton = new Button(manager.ContentManager.Load<Texture2D>("UI/White Close 2"), manager.ContentManager.Load<SpriteFont>("Fonts/ButtonFont"), "", new Vector2(1220, 25), 35, 35, Color.Red);
+            backGround = manager.ContentManager.Load<Texture2D>("UI/Scenes/Create_Module");
+            saveButton = new Button(manager.ContentManager.Load<Texture2D>("UI/Buttons/SaveButton"), null, null, new Vector2(540, 570), 211, 79, new Color(255, 255, 128));
+            switchPageModules = new SwitchPageModules(manager.ContentManager.Load<SpriteFont>("Fonts/SettingsNumbers"), new Vector2(115, 316), manager.ContentManager);
+        }
+
+        public void Load()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+
+            if (manager.InputManager.CheckIfLeftClick())
+            {
+                switchPageModules.CheckLeftClick(manager.InputManager.GetMousePosition());
+                if (exitButton.CheckLeftClick(manager.InputManager.GetMousePosition()))
+                {
+                    manager.SceneManager.RemoveScene();
+                }
+                else if (saveButton.CheckLeftClick(manager.InputManager.GetMousePosition()))
+                {
+                        manager.SceneManager.RemoveScene();
+                }
+            }
+
+            if (manager.InputManager.CheckIfCanPressKey(Keys.Escape))
+            {
+                manager.SceneManager.RemoveScene();
+            }
+
+            exitButton.Update(manager.InputManager.GetMousePosition());
+            saveButton.Update(manager.InputManager.GetMousePosition());
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        public void DrawStatic(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(backGround, new Rectangle(0, 0, manager.Camera.ScreenSize.Width, manager.Camera.ScreenSize.Height), Color.White);
+            saveButton.Draw(spriteBatch);
+            exitButton.Draw(spriteBatch);
+            switchPageModules.Draw(spriteBatch);
+        }
+
+    }
+}
---
+++ Client/Client/UI/CreateModules/Modules/ModuleData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.UI.CreateModules.Modules
+{
+    public enum ModuleType
+    {
+        Animal,
+        Plant
+    }
+
+    public class ModuleData
+    {
+        public Texture2D Texture;
+        private Rectangle Rect;
+        public ModuleType type;
+        public string Name;
+
+        public ModuleData(Texture2D texture, string name, ModuleType type)
+        {
+            Texture = texture;
+            Rect = new Rectangle(193, 210, 368, 200);
+            Name = name;
+            this.type = type;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Rect, Color.White);
+        }
+
+    }
+}
