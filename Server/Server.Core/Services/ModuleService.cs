3c1cf5bfd3e9c3e1021743a2b67f34231b14c8ce
Maciej Góralczyk <goralczykmaciek@gmail.com>
2025-11-01 13:53:41 +0100

    Changed migration data.

--- Server/Server.Core/Services/ModuleService.cs
+++ Server/Server.Core/Services/ModuleService.cs
@@ -47,9 +47,9 @@ namespace Server.Core.Services
             using (var dbContext = new ApplicationDBContext())
             {
                 ModuleDBEntity moduleDBEntity = new ModuleDBEntity
-                    (
-                        dto.Official, dto.Name, dto.Damage, dto.Agression, dto.ReproductionNeed, dto.Type, dto.GraphicalRepresentationID, dto.BehaviourIDs.ToArray()
-                    );
+                (
+                    dto.Official, dto.Name, dto.Damage, dto.Agression, dto.ReproductionNeed, dto.Type, dto.GraphicalRepresentationID, dto.BehaviourIDs.ToArray()
+                );
 
                 dbContext.Modules.Add(moduleDBEntity);
                 dbContext.SaveChanges();
