8be3762b2b5ba5b328f3a97e43bc2733e7bbff09
Maciej Góralczyk <83976515+Manie-K@users.noreply.github.com>
2025-11-01 13:54:36 +0100

    Changed migration data. (#127)

--- Server/Server.Core/Migrations/20251025185153_InitialCreate.cs
+++ Server/Server.Core/Migrations/20251025185153_InitialCreate.cs
@@ -38,7 +38,7 @@ namespace Server.Core.Migrations
                 columns: new[] { "ID", "Agression", "BehaviourIDs", "Damage", "GraphicalRepresentationID", "Name", "Official", "ReproductionNeed", "Type" },
                 values: new object[,]
                 {
-                    { -2, 7, new[] { 3, 2, 69 }, 3, 2, "Default module", true, 4, 2 },
+                    { -2, 70, new[] {101, 201, 307, 40 }, 30, 2, "Default module", true, 40, 2 },
                     { -1, 5, new int[0], 5, 1, "Human module", true, 5, 1 }
                 });
         }
