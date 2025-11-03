fb00d48fb87afffa0c6c2fae3e564e9be001ab77
Lachimek12 <michal_szkola20@wp.pl>
2025-11-03 21:42:17 +0100

    Added max hp and hunger, fixed behaviour lists

--- Client/Client/Scenes/CreateLobbyScene.cs
+++ Client/Client/Scenes/CreateLobbyScene.cs
@@ -81,6 +81,8 @@ namespace Client
                 parameters.Add(new ModuleParametersData("Damage", module.Damage.ToString(), 0));
                 parameters.Add(new ModuleParametersData("Aggresion", module.Aggresion.ToString(), 0));
                 parameters.Add(new ModuleParametersData("Reproduction Need", module.ReproductionNeed.ToString(), 0));
+                parameters.Add(new ModuleParametersData("Max Health", module.MaxHealth.ToString(), 0));
+                parameters.Add(new ModuleParametersData("Max Hunger", module.MaxHunger.ToString(), 0));
                 parameters.Add(new ModuleParametersData("Type", module.Type.ToString(), 0));
                 foreach (BehaviourDTO behviour in module.Behaviours)
                 {
