44fccb6225dd7dc2fdc5d1e7381c1368d9be93f0
franek73 <130841500+franek73@users.noreply.github.com>
2025-11-09 17:23:57 +0100

    another changes

--- Server/Server.Core/Behaviours/ReproduceBehaviour/PlantReproduceBehaviour.cs
+++ Server/Server.Core/Behaviours/ReproduceBehaviour/PlantReproduceBehaviour.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace Server.Core.Behaviours.ReproduceBehaviour
 {
-    internal class PlantReproduceBehaviour : ReproduceAccordingToReproductionNeedBehaviour
+    internal class PlantReproduceBehaviour : ReproduceBehaviourBase
     {
         //<inheritdoc/>
         public override EntityTypeEnum Type => EntityTypeEnum.Plant;
+        ///<inheritdoc/>
+        public override int DatabaseID => 403;
+        ///<inheritdoc/>
+        public override string Description => "Plant reproduce behaviour.";
     }
 }
