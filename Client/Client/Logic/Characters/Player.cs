2807d3ccb4c7bab433d3008dadf275d3938c6695
Lachimek12 <michal_szkola20@wp.pl>
2025-11-09 12:27:49 +0100

    Added player action cooldown

--- Client/Client/Logic/Characters/Player.cs
+++ Client/Client/Logic/Characters/Player.cs
@@ -128,11 +128,10 @@ namespace Client
                 }
             }
 
-            if (inputManager.CheckIfPressingKey(Keys.Space) && actionCooldown <= 0)
+            if (inputManager.CheckIfPressingKey(Keys.Space))
             {
                 HandleInteraction(InteractionType.Attack);
                 SetAnimation(1);
-                actionCooldown = PLAYER_ACTION_COOLDOWN;
             }
             if (inputManager.CheckIfPressingKey(Keys.E))
             {
@@ -166,6 +165,8 @@ namespace Client
 
         private void HandleInteraction(InteractionType interactionType)
         {
+            if (actionCooldown > 0) return;
+
             TargetEntity = clientManager.Entities.FirstOrDefault(e => e.Value.State.Position.Equals(map.GetTilePosition2D(Position.X, Position.Y))).Value;
             if (TargetEntity != null && TargetEntity.ModuleID != PlayerDTO.ModuleID)
             {
@@ -182,6 +183,8 @@ namespace Client
                         break;
                 }
             }
+
+            actionCooldown = PLAYER_ACTION_COOLDOWN;
         }
 
         private void AttackTarget()
