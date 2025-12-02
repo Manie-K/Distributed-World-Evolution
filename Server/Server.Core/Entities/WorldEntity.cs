80cc67553e9ddf9f5fca4080a1cf99c488ad16c0
Maciej Góralczyk <goralczykmaciek@gmail.com>
2025-12-02 23:48:32 +0100

    Removed comments.

--- Server/Server.Core/Entities/WorldEntity.cs
+++ Server/Server.Core/Entities/WorldEntity.cs
@@ -49,6 +49,10 @@ namespace Server.Core
         public void UpdateStateWithDelta(Position2D positionDelta, int healthDelta, int hungerDelta)
         {
             //TODO: change and add checks
+            if(positionDelta.X > 1 || positionDelta.X < -1 || positionDelta.Y > 1 || positionDelta.Y < -1)
+            {
+                Console.WriteLine($"Position delta might be incorrect. {positionDelta}");
+            }
             State.Position += positionDelta;
             State.Health += healthDelta;
             State.Hunger += hungerDelta;
